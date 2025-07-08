45396b7f15e5cd402dbbcdb66df43b51774fa35f
Md Hasan Molla <haasaan8@gmail.com>
2025-07-08 11:52:35 +0600

    info added

---
+++ src/ResumeOriginal.js
@@ -0,0 +1,170 @@
+
+
+const ResumeOriginal = () => {
+  return (
+    <div>
+      <div>
+        Md Hasan Molla
+        Electrical and Electronics Engineer (B. Sc)
+        IPCT Engineer (4 Years Diploma)
+        irhasanmolla@gmail.com
+        linkedin.com/in/irhasanmolla
+        Cell: +88 01758 810 927
+      </div>
+      {/* Professional Experience */}
+      <div>
+        <div>PROFESSIONAL EXPERIENCE</div>
+        <div>
+          <div>IT Manager</div>
+          <div>A.G. Associate Firm (CACC & ITP), Khulna, Bangladesh</div>
+          <div>
+            <div>March 2022- Present</div>
+            <ul>
+              <li>
+                <strong>Web Development: </strong> 
+                Designed, developed, and maintained the company website using HTML, CSS, and JavaScript, improving user experience and functionality.
+              </li>
+              <li>
+                <strong>Technical Support: </strong>
+                Provided hardware and software support, resolving issues swiftly to minimize downtime.
+              </li>
+              <li>
+                <strong>Team Leadership: </strong>
+                Supervised and trained a team of 2 IT staff, enhancing their technical skills and ensuring seamless IT operations.
+              </li>
+              <li>
+                <strong>Productivity Enhancement: </strong>
+                Leveraged Microsoft Office and internet tools to streamline business processes, resulting in a 20% increase in operational efficiency
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      {/* EDUCATION */}
+
+      <div>
+        <div>EDUCATION</div>
+        <div>
+          <div>
+            <strong>
+              Bachelor of Electrical and Electronics Engineering (E.E.E.)
+            </strong> ● Graduated: 2021
+          </div>
+          <div>Al-Madinah International University, Kuala Lumpur, Malaysia</div>
+        </div>
+        <div>
+          <div>
+            <strong>
+              Diploma in Instrumentation and Process Control Technology (I.P.C.T.) 
+            </strong> ● Graduated: 2014
+          </div>
+          <div>Khulna Polytechnic Institute, Khulna, Bangladesh</div>
+        </div>
+        
+      </div>
+
+      {/* PROJECTS */}
+
+      <div>
+        <div>PROJECTS</div>
+        <div>
+          <ul>Energy Saving in MEDIU Campus Using Home Automation</ul>
+          <ul>Design of a Wind Generator from a Modified Ceiling Fan Motor</ul>
+        </div>
+      </div>
+
+      {/* TECHNICAL SKILLS */}
+
+      <div>
+        <div>TECHNICAL SKILLS</div>
+        <div>
+          <ul><strong>Programming Languages: </strong> HTML, CSS, JavaScript, React, Python</ul>
+          <ul><strong>Graphic Design: </strong>Adobe Photoshop, Illustrator</ul>
+          <ul><strong>Office Suite: </strong>Microsoft Office Suite (Word, Excel, PowerPoint) - Advanced proficiency</ul>
+          <ul><strong>Video Editing</strong>Adobe Premiere Pro</ul>
+          <ul><strong>Web Development & SEO: </strong>Web design, SEO, and digital marketing (Facebook, Instagram)</ul>
+          <ul><strong>Languages</strong>Fluent in Bangla, English and Hindi</ul>
+        </div>
+      </div>
+
+      {/* PROFESSIONAL TRAINING */}
+
+      <div>
+        <div>PROFESSIONAL TRAINING</div>
+        <div>
+          <div>
+            <strong>
+              Compressor Machine (Sales, Installation & Service) 
+            </strong> ● Duration: 3 Months (2018)
+          </div>
+          <div>SCR Engineering & Services Sdn. Bhd., Malaysia</div>
+        </div>
+        <div>
+          <div>
+            <strong>
+              Consumer Electronics  
+            </strong> ● Duration: 3 Months (2014)
+          </div>
+          <div>Bangladesh Korea Technical Training Center, Dhaka, Bangladesh</div>
+        </div>
+        <div>
+          <div>
+            <strong>
+              Programmable Logic Controller (PLC) and Human Machine Interface (HMI)  
+            </strong> ● Duration: 3 Months (2014)
+          </div>
+          <div>KUUMRUL HRD Vision Limited, Dhaka, Bangladesh</div>
+        </div>
+        
+      </div>
+
+      {/* PROFESSIONAL SUMMARY */}
+
+      <div>
+        <div>PROFESSIONAL SUMMARY</div>
+        <div>
+          Detail-oriented Electrical and Electronics Engineer with over 11 years of experience in IT management, instrumentation, and process control. Expertise in developing IT solutions, maintaining systems, and enhancing operational productivity through technical training and support. Proven track record of leveraging engineering principles to solve complex problems and drive project success. Strong communicator and effective team player committed to continuous improvement and innovation.
+        </div>
+      </div>
+
+      {/* PERSONAL DETAILS */}
+
+      <div>
+        <div>PERSONAL DETAILS</div>
+        <div>
+          <ul><strong>Date of Birth: </strong>October 25, 1995</ul>
+          <ul><strong>Marital Status: </strong>Single</ul>
+          <ul><strong>Gender: </strong>Male</ul>
+          <ul><strong>Nationality: </strong>Bangladeshi</ul>
+          <ul><strong>Height: </strong>170 cm</ul>
+        </div>
+      </div>
+
+      {/* REFERENCES */}
+
+      <div>
+        <div>REFERENCES</div>
+        <div>
+          <div>
+            Ir Ts Dr Nohaidda Binti Sariff 
+            Senior Lecturer at Taylor's University, Malaysia
+            Email: nohaidda.sariff@taylors.edu.my,  |  Phone: +60 1773 75 847
+
+          </div>
+          <div>
+            Dr Nasir Ahmed Algeelani
+            Electrical and Smart Systems Engineering
+            Hanzehogeschool Groningen -Van Doorenveste, Zernikeplein 11 | The Netherlands 
+            Email: n.a.a.al.geelani@pl.hanze.nl   |   T: +31 (0) 50 595 6810 
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default ResumeOriginal
